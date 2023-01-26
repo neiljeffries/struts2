0c1c7048e7575e47637d0c75e4d3fca2981dda43
neiljeffries <neiljeffries@gmail.com>
2023-01-25 22:03:41 -0500

    initial

--- target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.struts.helloworld.action"},{"l":"org.apache.struts.helloworld.model"}];updateSearchResults();
--- target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.apache.struts.helloworld.action","l":"ExecAndWaitTestAction"},{"p":"org.apache.struts.helloworld.action","l":"HelloWorldAction"},{"p":"org.apache.struts.helloworld.model","l":"MessageStore"}];updateSearchResults();
